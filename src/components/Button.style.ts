9c5c5f22bcf22f4e1321fecf34fcca50192cb580
Mayra Amaral <mayracunha18@gmail.com>
2023-03-19 16:36:10 -0300

    refactor(styled|index): rename index title and finish button styled

--- src/components/Button.style.ts
+++ src/components/Button.style.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
@@ -17,5 +17,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) => `background-color: ${buttonVariants[props.variant]}`}
+  ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }}
 `;
