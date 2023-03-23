dcfa34aa626f9d5b15a917bcab98d0609a394fae
Mayra Amaral <mayracunha18@gmail.com>
2023-03-23 17:23:58 -0300

    feat(router): add router file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   );
---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { History } from "./pages/History";
+import { Home } from "./pages/Home";
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
